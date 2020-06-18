19000c93a031adf766237ba299c7724e8138189a
Tim Sazon <tim.sazon51@gmail.com>
2020-06-19 00:14:23 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+using Steamworks;
+using System;
+using System.Threading.Tasks;
+
+namespace SessionTicketGenerator
+{
+    class Program
+    {
+        [Verb("steam")]
+        public class SteamOptions
+        {
+
+            [Option('a', "app-id", Required = true, HelpText = "App ID.")]
+            public uint AppId { get; set; }
+        }
+
+        static async Task Main(string[] args)
+        {
+            await Parser.Default.ParseArguments<SteamOptions, object>(args)
+                  .WithParsedAsync<SteamOptions>(async o =>
+                  {
+                      SteamClient.Init(o.AppId);
+                      var ticket = await SteamUser.GetAuthSessionTicketAsync();
+                      SteamClient.Shutdown();
+
+                      var hexTicket = BitConverter.ToString(ticket.Data).Replace("-", "");
+                      Console.WriteLine($"Session ticket = {hexTicket}");
+                  });
+        }
+    }
+}
